ec40df827be141f92cfdb65b6b2a3aade88acf27
Peter Kamlewechi <peter.kamlewechi@bison.howard.edu>
2020-11-15 00:32:37 +0000

    made chances to shm.processes

--- shm_processes.c
+++ shm_processes.c
@@ -5,67 +5,103 @@
 #include  <sys/shm.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <semaphore.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>	
+#include <sys/mman.h>
 
-void  ClientProcess(int []);
+void depositMoney(int* bankAccount);
+void withdrawMoney(int* bankAccount);
 
-int  main(int  argc, char *argv[])
-{
-     int    ShmID;
-     int    *ShmPTR;
-     pid_t  pid;
-     int    status;
+int  main(int  argc, char *argv[]){
+    int    ShmID;
+    int    *ShmPTR;
+    pid_t  pid;
+    int    status;
+    int fd ,i , nloop=5,zero=0,*counter_ptr;
+    sem_t *mutex; 
+  
+  fd = open("log.txt",O_RDWR|O_CREAT,S_IRWXU);
+  write(fd,&zero,sizeof(int));
+  counter_ptr = mmap(NULL,sizeof(int),PROT_READ |PROT_WRITE,MAP_SHARED,fd,0);
+  close(fd);
 
-     if (argc != 5) {
-          printf("Use: %s #1 #2 #3 #4\n", argv[0]);
-          exit(1);
-     }
+  /* create, initialize semaphore */
+ if ((mutex = sem_open("examplesaaaebbmaphore", O_CREAT, 0644, 1)) == SEM_FAILED) {
+    perror("semaphore initilization");
+    exit(1);
+  }
+  
+    pid = fork();
+    if (pid > 0) {
+        for (i = 0; i>-1; i++){
+            sleep(rand()%6);
+            printf("Dear Old Dad: Attempting to Check Balance\n");\
+            sem_wait(mutex);
+            int randomNumber = rand()%101;
+            if (randomNumber % 2 == 0){
+              if (*counter_ptr<100){
+                depositMoney(counter_ptr);
+              } else {
+                printf("Dear old Dad: Thinks Student has enough Cash ($%d)\n", *counter_ptr);
+              }
+            }else{
+              printf("Dear Old Dad: Last Checking Balance = $%d\n", *counter_ptr);
+            }
+            sem_post(mutex);              
+        }
+        
+        exit(1);
+    }
+    else if (pid == 0) {
+        for (i = 0; i>-1; i++){
+            sleep(rand()%6);
+            printf("Poor Student: Attempting to Check Balance\n");  
+            sem_wait(mutex);
+            int randomNumber = rand();
+            if (randomNumber%2 == 0){
+              withdrawMoney(counter_ptr);
+            }else{
+              printf("Poor Student: Last Checking Balance = $%d\n", *counter_ptr);
+            }
+            sem_post(mutex);
+            
+        }
+        printf("   Client is about to exit\n");
+        exit(0);
+    }
 
-     ShmID = shmget(IPC_PRIVATE, 4*sizeof(int), IPC_CREAT | 0666);
-     if (ShmID < 0) {
-          printf("*** shmget error (server) ***\n");
-          exit(1);
-     }
-     printf("Server has received a shared memory of four integers...\n");
-
-     ShmPTR = (int *) shmat(ShmID, NULL, 0);
-     if (*ShmPTR == -1) {
-          printf("*** shmat error (server) ***\n");
-          exit(1);
-     }
-     printf("Server has attached the shared memory...\n");
-
-     ShmPTR[0] = atoi(argv[1]);
-     ShmPTR[1] = atoi(argv[2]);
-     ShmPTR[2] = atoi(argv[3]);
-     ShmPTR[3] = atoi(argv[4]);
-     printf("Server has filled %d %d %d %d in shared memory...\n",
-            ShmPTR[0], ShmPTR[1], ShmPTR[2], ShmPTR[3]);
-
-     printf("Server is about to fork a child process...\n");
-     pid = fork();
-     if (pid < 0) {
-          printf("*** fork error (server) ***\n");
-          exit(1);
-     }
-     else if (pid == 0) {
-          ClientProcess(ShmPTR);
-          exit(0);
-     }
-
-     wait(&status);
-     printf("Server has detected the completion of its child...\n");
-     shmdt((void *) ShmPTR);
-     printf("Server has detached its shared memory...\n");
-     shmctl(ShmID, IPC_RMID, NULL);
-     printf("Server has removed its shared memory...\n");
-     printf("Server exits...\n");
-     exit(0);
+    wait(&status);
+    printf("Server has detected the completion of its child...\n");
+    printf("Server has detached its shared memory...\n");
+    printf("Server has removed its shared memory...\n");
+    printf("Server exits...\n");
+    exit(0);
 }
 
-void  ClientProcess(int  SharedMem[])
-{
-     printf("   Client process started\n");
-     printf("   Client found %d %d %d %d in shared memory\n",
-                SharedMem[0], SharedMem[1], SharedMem[2], SharedMem[3]);
-     printf("   Client is about to exit\n");
+void depositMoney(int* bankAccount){
+  int localBalance = *bankAccount;
+  int amount = rand() % 101;
+  if (amount%2 == 0){
+    localBalance+= amount;
+    printf("Dear old Dad: Deposits $%d / Balance = $%d\n", amount, localBalance);
+    *bankAccount = localBalance;
+  }
+  else{
+    printf("Dear old Dad: Doesn't have any money to give\n");
+  }
+}
+
+void withdrawMoney(int* bankAccount){
+  int localBalance = *bankAccount;
+  int need = rand() % 51;
+  printf("Poor Student needs $%d\n", need);
+  if (need <= localBalance){
+    localBalance-=need;
+    printf("Poor Student: Withdraws $%d / Balance = $%d\n", need, localBalance);
+    *bankAccount = localBalance;
+  }else{
+    ("Poor Student: Not Enough Cash ($%d)\n", localBalance);
+  }
 }
